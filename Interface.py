2013fa7516780050b8f624ec6750411cb8a09f6a
Gabriel Buhrer <gabrielpb1@al.insper.edu.br>
2022-11-04 17:52:26 -0300

    Interface

---
+++ Interface.py
@@ -0,0 +1,23 @@
+print("Olá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!")
+
+print()
+
+nome = input("Qual seu nome? ")
+
+print()
+
+print("Ok {0}, você tem direito a pular 3 vezes e 2 ajudas!".format(nome))
+print("As opções de resposta são 'A', 'B', 'C', 'D', 'ajuda', 'pula' e 'parar'!")
+
+print()
+
+print("Aperte ENTER para continuar...")
+
+print()
+
+print("O jogo ja vai começar! Lá vem a primeira questão!")
+
+print("Vamos começar com questões do nível FÁCIL!")
+print("Aperte ENTER para continuar...")
+
+
