df62743d797da51b4ef9e77889f2f94c44926bac
DanielMarcoD <danielmd3@al.insper.edu.br>
2022-11-10 18:47:43 -0300

    gera ajuda

--- gera_ajuda.py
+++ gera_ajuda.py
@@ -9,8 +9,7 @@ def gera_ajuda(questao):
         if valores!=certa:
             questões_para_sortear.append(valores)
 
-    #numero_questoes_sorteadas=random.randint(1,2)
-    numero_questoes_sorteadas=1
+    numero_questoes_sorteadas=random.randint(1,2)
     if numero_questoes_sorteadas==1:
         numero=random.randint(0,2)
         questao_sorteada=questões_para_sortear[numero]
@@ -18,16 +17,12 @@ def gera_ajuda(questao):
         resp="DICA:"+"\n"+'Opções certamente erradas: '+ Errada
         return resp
     else:
-        while len(questões_ja_sorteadas)<=2:
+        while len(questões_ja_sorteadas)<2:
             numero=random.randint(0,2)
             questao_sorteada=questões_para_sortear[numero]
             if questao_sorteada not in questões_ja_sorteadas:
                 questões_ja_sorteadas.append(questao_sorteada)
-        Errada=questao['opcoes'][questões_ja_sorteadas][0]
-        Errada1=questao['opcoes'][questões_ja_sorteadas][1]        
-        resp="DICA:"+"\n"+'Opções certamente erradas: '+ Errada + Errada1
-        return resp         
-                
-                
-
-
+        Errada=questao['opcoes'][questões_ja_sorteadas[0]]
+        Errada1=questao['opcoes'][questões_ja_sorteadas[1]]        
+        resp="DICA:"+"\n"+'Opções certamente erradas: '+ Errada +' | '+ Errada1
+        return resp
